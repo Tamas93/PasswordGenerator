0b5f878758b9680d8e4052da3b3e69e2b6db5fa7
Tamas <borbelytam9304@gmail.com>
2022-01-10 10:10:34 +0100

    All files upload

---
+++ password_generator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace password_generator
+{
+    class Program
+    {
+        static string Passworld(int x)
+        {
+            Random rnd = new Random();
+            string chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            StringBuilder build = new StringBuilder();
+
+            while(0 < x)
+            {
+                build.Append(chars[rnd.Next(chars.Length)]);
+                x--;
+            }
+            return build.ToString();
+        }
+        static void Main(string[] args)
+        {
+
+            Console.Write("Hello, Ez egy random jelszó generátor program!\nKérlek Add meg hogy hány jegyből álljon a jelszó:");
+            int a = int.Parse(Console.ReadLine());
+            string password = Passworld(a);
+            Console.WriteLine("A jelszód: {0}",password);
+
+            Console.ReadKey();
+        }
+        
+    }
+   
+}
